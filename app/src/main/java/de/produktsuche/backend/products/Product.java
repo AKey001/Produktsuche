a788442ba3fb74df22f357701ee1172a8c8d9005
Anton <anton.kiessling@web.de>
2021-12-08 17:56:06 +0100

    RecyclerViewAdapter, Product, strings.xml, content_list.xml

--- app/src/main/java/de/produktsuche/backend/products/Product.java
+++ app/src/main/java/de/produktsuche/backend/products/Product.java
@@ -1,6 +1,40 @@
 package de.produktsuche.backend.products;
 
 public class Product {
-    //TODO Product
+    private String name;
+    private String store;
+    private int availability;
+    private double price;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStore() {
+        return store;
+    }
+
+    public void setStore(String store) {
+        this.store = store;
+    }
+
+    public int getAvailability() {
+        return availability;
+    }
+
+    public void setAmount(int availability) {
+        this.availability = availability;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
--- app/src/main/java/de/produktsuche/backend/products/RecyclerViewAdapter.java
+++ app/src/main/java/de/produktsuche/backend/products/RecyclerViewAdapter.java
@@ -1,32 +1,80 @@
 package de.produktsuche.backend.products;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
-public class RecyclerViewAdapter extends RecyclerView.Adapter {
-    private List<Product> items;
+import de.produktsuche.R;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+    private final List<Product> items;
 
     public RecyclerViewAdapter(List<Product> products) {
         items = products;
     }
 
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView textViewProduct;
+        private final TextView textViewStore;
+        private final TextView textViewAvailability;
+        private final TextView textViewPrice;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            textViewProduct = itemView.findViewById(R.id.textViewProduct);
+            textViewStore = itemView.findViewById(R.id.textViewShop);
+            textViewAvailability = itemView.findViewById(R.id.textViewAvailability);
+            textViewPrice = itemView.findViewById(R.id.textViewPrice);
+        }
+
+        public TextView getTextViewProduct() {
+            return textViewProduct;
+        }
+
+        public TextView getTextViewStore() {
+            return textViewStore;
+        }
+
+        public TextView getTextViewAvailability() {
+            return textViewAvailability;
+        }
+
+        public TextView getTextViewPrice() {
+            return textViewPrice;
+        }
+    }
+
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_item, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Product product = items.get(position);
 
+        holder.getTextViewProduct().setText(product.getName());
+        holder.getTextViewStore().setText(product.getStore());
+        if (product.getAvailability() > 0) {
+            holder.getTextViewAvailability().setText(product.getAvailability() + " reserviert");
+        } else {
+            holder.getTextViewAvailability().setText("noch nicht bestätigt");
+        }
+        holder.getTextViewPrice().setText(String.valueOf(product.getPrice()));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return items.size();
     }
 }
